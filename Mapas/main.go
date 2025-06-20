2d517c92374328931d0bf9cd54e6e13983a7c67e
JrGustavo <gadolfoalvarado@gmail.com>
2025-06-19 22:08:33 -0500

    Update Code

--- Mapas/main.go
+++ Mapas/main.go
@@ -1,1 +1,43 @@
-package Mapas
+package main
+
+import (
+	"fmt"
+	"maps"
+)
+
+func main() {
+
+	mapa := make(map[string]int)
+	mapa["Amin"] = 4
+	mapa["espinoza"] = 8
+
+	fmt.Println("mapa: ", mapa)
+
+	version1 := mapa["espinoza"]
+	fmt.Println("version 1: ", version1)
+
+	version2 := mapa["Amin"]
+	fmt.Println("version 2: ", version2)
+
+	fmt.Println("longitud del mapa: ", len(mapa))
+
+	_, dato := mapa["Amin"]
+	fmt.Println("data: ", dato)
+
+	delete(mapa, "amin")
+	fmt.Println("mapa: ", mapa)
+
+	clear(mapa)
+	fmt.Println("mapa: ", mapa)
+
+	nuevoMapa1 := map[string]int{"Amin": 4, "espinoza": 8}
+	fmt.Println("nuevo mapa: ", nuevoMapa1)
+
+	nuevoMapa2 := map[string]int{"Amin": 4, "espinoza": 8}
+	fmt.Println("nuevo mapa: ", nuevoMapa2)
+
+	if maps.Equal(nuevoMapa1, nuevoMapa2) {
+		fmt.Println("los mapas son iguales")
+	}
+
+}
